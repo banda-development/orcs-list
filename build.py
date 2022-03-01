9b7326d85a8767475f7c490691a1eb12d2d8f8d1
mogu4iy <mogu4iy.kotygoroshko@gmail.com>
2022-03-01 17:17:20 +0200

    fix: Fix links

--- build.py
+++ build.py
@@ -4,8 +4,8 @@ def main():
         data = index_file.read()
         for i in range(1, 64):
             build_data = data.replace("data_0.csv", f"data_{i}.csv")
-            build_data = build_data.replace("/1.html", f"/{i+1}.html")
-            build_data = build_data.replace("/first.html",f"/{i-1}.html")
+            build_data = build_data.replace("/index", f"{i-1}")
+            build_data = build_data.replace("/1", f"{i+1}")
             with open(f"{i}.html", "w", encoding="utf-8") as build_file:
                 build_file.write(build_data)
 
